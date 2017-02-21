224225731aebe9ba914d853fec712465ad1ead00
Lucas Sacramento <lucas.arki@gmail.com>
2017-02-21 20:47:35 +0000

    Created Accept Loan Route and Component

--- modules/dashboards/client/config/react-dahsboard.client.route.jsx
+++ modules/dashboards/client/config/react-dahsboard.client.route.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
 
-import { Timeline, RequestLoan } from '../routes/index.jsx'
+import { Timeline, RequestLoan, AcceptLoan } from '../routes/index.jsx'
 
 class App extends Component {
   render() {
     return (
       <Router history={browserHistory}>
         <Route path='/request' component={RequestLoan} />
-        <Route path='/timeline' component={Timeline} />
+        <Route path='dashboards/timeline' component={Timeline} />
+        <Route path='/accept' component={AcceptLoan} />
       </Router>
     )
   }
